88aa275bc4422a8b2a7444f0d5fb5288d913fde5
Alesha Ray <alesha.ray@g.austincc.edu>
2017-03-11 00:11:13 -0600

    Added constant string arrays two hold numbers in text form

--- driver.cpp
+++ driver.cpp
@@ -3,10 +3,18 @@
 
 using namespace std;
 
+const string ones[19] = {"zero", "one", "two", "three", "four", "five",
+						 "six", "seven", "eight", "nine" "ten",
+						 "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+						 "sixteen", "seventeen", "eighteen", "nineteen"},
+			 tens[10] = {"", "", "twenty", "thirty", "forty", "fifty",
+					   "sixty", "seventy", "eighty", "ninety"};
+			 endings[5] = {"hundred", "thousand", "million", "billion", "trillion"};
+
+
 int main()
 {
 
-	cout << "Hello World!" << endl;
 
 	return 0;
 }
