1b64d57a7b80ab7dc4784558b85bb54b6e01d9df
Alesha Ray <alesha.ray@g.austincc.edu>
2017-03-11 20:19:55 -0600

    Wrote function for hundreds place

--- driver.cpp
+++ driver.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
 #include<ctime>
+#include<cmath>
 
 using namespace std;
+
+const string ones[19] = {"zero", "one", "two", "three", "four", "five",
+						 "six", "seven", "eight", "nine" "ten",
+						 "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+						 "sixteen", "seventeen", "eighteen", "nineteen"},
+			 tens[10] = {"", "", "twenty", "thirty", "forty", "fifty",
+					   "sixty", "seventy", "eighty", "ninety"},
+			 endings[6] = {"", "hundred", "thousand", "million", "billion", "trillion"};
+	string numToText = "";
 
 int main()
 {
 	srand(time(NULL));
 
-	const string ones[19] = {"zero", "one", "two", "three", "four", "five",
-							 "six", "seven", "eight", "nine" "ten",
-							 "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-							 "sixteen", "seventeen", "eighteen", "nineteen"},
-				 tens[10] = {"", "", "twenty", "thirty", "forty", "fifty",
-						   "sixty", "seventy", "eighty", "ninety"},
-				 endings[5] = {"hundred", "thousand", "million", "billion", "trillion"};
-		string numToText = "";
 
-	int num = 0 + rand() % 999999999999;
+
+int hunPlace(int, int);
+
+//	int num = 0 + rand() % 999999999999;
+	int num = 4592;
+	cout << num << endl;
 
 	if(num == 0)
 		cout << ones[0] << endl;
@@ -31,13 +38,14 @@ int main()
 				length++;
 			}
 
-			int ending;
-//				places = length;
-			while(length > 0)
+			int ending,
+				places = length;
+			while(places > 0)
 			{
-				length -= 3;
+				places -= 3;
 				ending++;
 			}
+
 
 
 
@@ -47,5 +55,16 @@ int main()
 	}
 
 	return 0;
+}
+
+int hunPlace(int number, int length)
+{
+	int hundreds = number / pow(10, length-1);
+	cout << hundreds << endl;
+
+	numToText += ones[hundreds];
+	number -= (hundreds * pow(10, length-1));
+
+	return number;
 }
 
